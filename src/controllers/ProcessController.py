87926b151e1b96fe49480915ae7f96c61529e40c
Mustafa Elkady <MustaphaElkady@gmail.com>
2025-11-15 18:23:31 +0200

    create processcontoller

---
+++ src/controllers/ProcessController.py
@@ -0,0 +1,9 @@
+from .BaseController import BaseController
+from .ProjectController import ProjectController
+
+class ProcessController(BaseController):
+    def __init__(self, project_id: str):
+        super().__init__()
+
+        self.project_id = project_id
+        self.project_path = ProjectController().get_project_path(project_id = project_id)
