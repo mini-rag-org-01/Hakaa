a577e235f1397752d2ad227e3eab2885577247bf
Mustafa Elkady <mustaphaelkady@gmail.com>
2026-04-25 20:19:23 +0300

    init vectordb folder

--- src/main.py
+++ src/main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 from routes import base 
 from routes import data 
 from motor.motor_asyncio import AsyncIOMotorClient
-
 #to call database name/url
 from helpers.config import get_settings
+from stores.LLM.LLMProviderFActory import LLMProviderfactory
+
+
 
 app = FastAPI()
 
@@ -15,7 +17,16 @@ async def startup_db_client():
     app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
     app.db_client =  app.mongo_conn [settings.MONGODB_DATABASE]
 
+    llm_provider_factory = LLMProviderfactory(settings)
 
+    #generate client
+    app.generation_client = llm_provider_factory.create(provider=settings.GENERATION_BACKEND)
+    app.generation_client.set_generation_model(model_id=settings.GENERATION_MODEL_ID)
+
+    # embedding client 
+    app.embedding_client = llm_provider_factory.create(provider=settings.EMBEDDING_BACKEND)
+    app.embedding_client.set_embedding_model(model_id=settings.EMBEDDING_MODEL_ID,
+                                             embedding_size=settings.EMBEDDING_MODEL_SIZE)
 @app.on_event("shutdown")
 async def shutdown_db_client():
     app.mongo_conn.close()
