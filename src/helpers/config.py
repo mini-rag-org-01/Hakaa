afcd927ca63d43b09a14c34938831d161e62217c
Mustafa Elkady <mustaphaelkady@gmail.com>
2026-05-02 12:28:23 +0300

    templates files

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -34,7 +34,11 @@ class Settings(BaseSettings):
     VECTOR_DB_DISTANCE_METHOD: str = None
 
     PRIMARY_LANG: str = "en"
-    DEFAULT_LANG: str = "en"
+    DEFAULT_LANG = "en"
+
+
+
+
     class Config:
         env_file = ".env"
 
