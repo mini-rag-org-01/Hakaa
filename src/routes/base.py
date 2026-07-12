ce7cb80b1b0699d53fc5ee1d4df2aa5c9c6beee7
Mustafa Elkady <mustaphaelkady@gmail.com>
2026-07-12 10:23:52 +0300

    Update base.py

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, APIRouter, Depends
 import os
 from helpers.config import get_settings, Settings
+from datetime import datetime
 
 base_router = APIRouter(
     prefix="/api/v1",
@@ -16,4 +17,5 @@ async def welcome(app_settings: Settings = Depends(get_settings)):
     return {
         "app_name": app_name,
         "app_version": app_version,
+        "datetime": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     }
