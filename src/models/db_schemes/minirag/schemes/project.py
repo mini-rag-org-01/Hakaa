9a93b4b609cdbca2227436a985a7eb00536276ff
gihub <github_user@ip-172-26-7-208.eu-north-1.compute.internal>
2026-08-21 01:11:43 +0000

    feat(projects): add project metadata

--- src/models/db_schemes/minirag/schemes/project.py
+++ src/models/db_schemes/minirag/schemes/project.py
@@ -1,6 +1,6 @@
 from time import timezone
 from .minirag_base import SQLAlchemyBase
-from sqlalchemy import Column, Integer, DateTime, func
+from sqlalchemy import Column, Integer, DateTime, func, String, Boolean, Text, false
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 
@@ -12,6 +12,21 @@ class Project(SQLAlchemyBase):
     # define Columns name
     project_id = Column(Integer, primary_key=True, autoincrement= True)
     project_uuid = Column(UUID(as_uuid=True), default=uuid.uuid4, unique= True, nullable=False) 
+    project_name = Column(String(150), nullable=True)
+    project_description = Column(Text, nullable=True)
+
+    is_public = Column(
+        Boolean,
+        nullable=False,
+        server_default=false(),
+        index=True,
+    )
+    project_status = Column(
+        String(20),
+        nullable=False,
+        server_default="draft",
+        index=True,
+    )
 
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=True)
